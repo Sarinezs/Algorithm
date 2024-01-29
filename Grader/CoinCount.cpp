1f11775801ba9d79b771e9bbcfbe9193c5704687
Suriya Meerua <a.edr.edm.ed@gmail.com>
2024-01-29 15:46:51 +0700

    success

---
+++ Grader/CoinCount.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int subset(int a[], int x[], int k, int n){
+    if(k == n){
+        int sum = 0;
+        for(int i = 0; i<n-1; i++){
+            if(x[i] == x[i+1]){
+                x[i+1] = 0;
+            }
+        }
+
+        for(int i = 0; i<n; i++){
+            if(x[i] == 1){
+                sum += a[i];
+            }
+        }
+        
+        return sum;
+        
+    }
+    else{
+        x[k] = 1;
+        int sum1 = subset(a, x, k+1, n);
+        x[k] = 0;
+        int sum2 = subset(a, x, k+1, n);
+
+        int max = (sum1 > sum2) ? sum1 : sum2;
+
+        return max;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n], b[n];
+    for(int i = 0; i<n; i++){
+        cin>>a[i];
+    }
+
+   
+    cout<<subset(a, b, 0, 10);
+    
+}
